4c21e50660247c57e0b7e075a0689992fd406808
monkeytitle <monkeytitle@hotmail.com>
2023-06-14 20:50:41 +0700

    add: navigation button succesfully

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,37 @@
-import React from 'react'
+"use client";
+import { Container, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import React from "react";
+import "./globals.css";
 type Props = {
-  children: React.ReactNode
-}
-function Layout({children}: Props) {
+  children: React.ReactNode;
+};
+
+function Layout({ children }: Props) {
   return (
-    <div>layout</div>
-  )
+    <div>
+      <Grid container spacing={2} id ="navbar-navigation">
+        <Grid item lg={6}></Grid>
+        <Grid item lg={6}>
+          <Grid container spacing={5} >
+            <Grid item>
+              <p>Activity</p>
+            </Grid>
+            <Grid item>
+              <p>Info</p>
+            </Grid>
+            <Grid item>
+              <p>Project</p>
+              </Grid>
+            <Grid item>
+              <p>Resume</p>
+              </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+      <div>{children}</div>
+    </div>
+  );
 }
 
-export default Layout
+export default Layout;
