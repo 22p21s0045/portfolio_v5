1ececdb7ccc8a61358e71313ec6d6a3ace031fed
monkeytitle <monkeytitle@hotmail.com>
2023-06-17 11:39:21 +0700

    add:react spring

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,33 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import Image from "next/image";
+import styles from "./page.module.css";
 import Grid from "@mui/material/Grid";
+import { useSpring, animated } from "@react-spring/web";
+
 export default function Home() {
+  const animatedStyles = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+    config: { duration: 500 },
+  });
   return (
     <div>
       <Grid container>
         <Grid item lg={6}>
-          </Grid>
-        <Grid item lg={6}>
-          </Grid>
+          <animated.div style={animatedStyles}>
+            <Image
+              src={"/svg/landing/hello.svg"}
+              alt="Logo"
+              width={465}
+              height={135}
+              style={{ position: "relative", top: "18px" }}
+            />
+            <h1 style={{ color: "white" }}>Hello worldssss</h1>
+          </animated.div>
+        </Grid>
+        <Grid item lg={6}></Grid>
       </Grid>
+      <h1>ggg</h1>
     </div>
-  )
+  );
 }
